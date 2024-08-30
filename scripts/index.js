7d05bf4edf5ccf31e558012a94c2ce7bb7d63970
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-08-30 15:43:45 +0100

    added js file to index.html, imported data dynamically.

---
+++ scripts/index.js
@@ -0,0 +1,90 @@
+// select the container that has all products :
+const productsGrid = document.querySelector('.products-grid');
+
+const productsList = [{
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+        stars: 4.5,
+        count: 87
+    },
+    priceCents: 1090
+},
+{
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+        stars: 4,
+        count: 127
+    },
+    priceCents: 2095
+},
+{
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+        stars: 4.5,
+        count: 56
+    },
+    priceCents: 799
+}];
+
+let productsGridHtml = '';
+
+productsList.forEach(i => {
+    productsGridHtml += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src=${i.image}>
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${i.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${i.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${i.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(i.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+    `
+});
+
+productsGrid.innerHTML = productsGridHtml
+
+/* productsList.forEach(i => console.log(i.rating.count));
+productsList.forEach(i => console.log(i.rating.stars)); */
