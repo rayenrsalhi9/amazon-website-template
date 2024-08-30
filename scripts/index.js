04337226017c7f00280ed65f98fc5d90d8c17654
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-08-30 16:30:56 +0100

    fixed the import data by linking 2 js files.

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,9 @@
-
 // select the container that has all products :
 const productsGrid = document.querySelector('.products-grid');
 
-const productsList = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},
-{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
-
 let productsGridHtml = '';
 
-productsList.forEach(i => {
+products.forEach(i => {
     productsGridHtml += `
         <div class="product-container">
           <div class="product-image-container">
@@ -85,9 +56,4 @@ productsList.forEach(i => {
     `
 });
 
-productsGrid.innerHTML = productsGridHtml
-
-
-
-/* productsList.forEach(i => console.log(i.rating.count));
-productsList.forEach(i => console.log(i.rating.stars)); */
+productsGrid.innerHTML = productsGridHtml;
