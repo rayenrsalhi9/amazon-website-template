67e096fce737697feb6a9dc873a9a85089796024
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-04 00:43:35 +0100

    generated delivery options and added their html to checkout page dynamically.

---
+++ data/deliveryOptions.js
@@ -0,0 +1,19 @@
+const deliveryOptions = [
+    {
+        id: '1',
+        deliveryDuration: 7,
+        deliveryCents: 0 
+    },
+    {
+        id: '2',
+        deliveryDuration: 3,
+        deliveryCents: 499
+    },
+    {
+        id: '3',
+        deliveryDuration: 1,
+        deliveryCents: 999
+    }
+];
+
+export default deliveryOptions;
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,6 @@
-import { cart, removeItemFromCart, saveToLocalStorage, updateQuantity } from '../data/cart.js';
+import { cart, removeItemFromCart, saveToLocalStorage } from '../data/cart.js';
+import deliveryOptions from '../data/deliveryOptions.js';
 import { products } from '../data/products.js';
-
-// external library :
-const todaysDate = dayjs();
-const todaysDateFormat = todaysDate.format('dddd,MMMM D');
-
-const sevenDaysLater = todaysDate.add(7, 'day').format('dddd,MMMM D');
-const threeDaysLater = todaysDate.add(3, 'day').format('dddd,MMMM D');
-const oneDayLater = todaysDate.add(1, 'day').format('dddd,MMMM D');
-
-
 
 // selecting items :
 const orderSummary = document.querySelector('.order-summary');
@@ -17,21 +8,24 @@ const homeLink = document.querySelector('.return-to-home-link');
 
 addProductsToCheckout(orderSummary);
 
-const cartItems = document.querySelectorAll('.cart-item-container');
+/* const cartItems = document.querySelectorAll('.cart-item-container');
 cartItems.forEach(item => {
 
     const deliveryDateDiv = item.querySelector('.delivery-date');
     const radioButtons = item.querySelectorAll('.delivery-option-input');
     radioButtons.forEach(radio => {
-        radio.addEventListener('change', () => {
             if (radio.checked) {
                 const selectedDate = radio.dataset.date;
                 deliveryDateDiv.innerHTML = `Delivery date: ${selectedDate}`;
             }
-        })
-    })
-    
-}); 
+            radio.addEventListener('change', () => {
+                if (radio.checked) {
+                    const selectedDate = radio.dataset.date;
+                    deliveryDateDiv.innerHTML = `Delivery date: ${selectedDate}`;
+                }
+            });
+        });
+    }); */ 
 
 // update quantity in home link :
 let linkQuantity = 0;
@@ -39,7 +33,6 @@ addQuantityToHomeLink();
 
 // add event listeners to delete buttons :
 const deleteButtons = document.querySelectorAll('.delete-quantity-link');
-
 deleteButtons.forEach(btn => {
     btn.addEventListener('click', () => {
         
@@ -89,27 +82,36 @@ function addProductsToCheckout(orderSummary) {
 
     let orderSummaryHtml = '';
 
-    for (let i in cart) {
+    cart.forEach(el => {
 
-        const productId = cart[i].productId;
+        const productId = el.productId;
 
         let matchingProduct;
 
-        for (let i in products) {
-            if (productId === products[i].id) {
-                matchingProduct = products[i];
+        products.forEach(i => {
+            if (productId === i.id) {
+                matchingProduct = i;
             }
-        }
+        });
 
         const productImage = matchingProduct.image;
         const productPrice = matchingProduct.priceCents;
         const productName = matchingProduct.name;
 
+        const deliveryOptionId = el.deliveryOptionId;
+        let matchingIdItem;
+        deliveryOptions.forEach(option => {
+            if (option.id === deliveryOptionId) matchingIdItem = option;
+        });
+
+        const todaysDate = dayjs();
+        const deliveryDate = todaysDate.add(matchingIdItem.deliveryDuration, 'day').format('dddd,MMMM D');
+
         orderSummaryHtml += `
 
             <div class="cart-item-container js-${matchingProduct.id}">
 
-                <div class="delivery-date"> Delivery date: ${sevenDaysLater}</div>
+                <div class="delivery-date"> Delivery date: ${deliveryDate}</div>
 
                 <div class="cart-item-details-grid">
 
@@ -123,7 +125,7 @@ function addProductsToCheckout(orderSummary) {
 
                         <div class="product-quantity">
 
-                            <span> Quantity: <span class="quantity-label">${cart[i].quantity}</span> </span>
+                            <span> Quantity: <span class="quantity-label">${el.quantity}</span> </span>
                             <span class="update-quantity-link link-primary" data-id="${matchingProduct.id}"> Update </span>
                             <input class="quantity-input hidden">
                             <span class="save-quantity-link hidden link-primary">Save</span>
@@ -136,36 +138,14 @@ function addProductsToCheckout(orderSummary) {
 
                         <div class="delivery-options-title"> Choose a delivery option: </div>
 
-                        <div class="delivery-option">
-                            <input type="radio" checked class="delivery-option-input" name="delivery-option-${cart[i].productId}" data-date="${sevenDaysLater}">
-                            <div>
-                                <div class="delivery-option-date"> ${sevenDaysLater} </div>
-                                <div class="delivery-option-price"> FREE Shipping </div>
-                            </div>
-                        </div>
-
-                        <div class="delivery-option">
-                            <input type="radio" class="delivery-option-input" name="delivery-option-${cart[i].productId}" data-date="${threeDaysLater}">
-                            <div>
-                                <div class="delivery-option-date"> ${threeDaysLater} </div>
-                                <div class="delivery-option-price"> $4.99 - Shipping </div>
-                            </div>
-                        </div>
-
-                        <div class="delivery-option">
-                            <input type="radio" class="delivery-option-input" name="delivery-option-${cart[i].productId}" data-date="${oneDayLater}">
-                            <div>
-                                <div class="delivery-option-date"> ${oneDayLater} </div>
-                                <div class="delivery-option-price"> $9.99 - Shipping </div>
-                            </div>
-                        </div>
+                        ${generateDeliveryOptions(matchingProduct, el)}
 
                     </div>
                 </div>
             </div>
 
 `;
-};
+    })
 
     orderSummary.innerHTML = orderSummaryHtml;
 }
@@ -193,4 +173,35 @@ function updateSavedQuantity(updateButtonId, inputAreaValue) {
 
     saveToLocalStorage();
 
+}
+
+function generateDeliveryOptions(matchingProduct, el) {
+
+    let deliveryOptionsHtml = ''
+
+    deliveryOptions.forEach(option => {
+
+        const todaysDate = dayjs();
+        const deliveryDate = todaysDate.add(option.deliveryDuration, 'day').format('dddd,MMMM D');
+        const deliveryCost = option.deliveryCents === 0 ? 'FREE' : `$${(option.deliveryCents / 100).toFixed(2)} - `;
+
+        const isChecked = option.id === el.deliveryOptionId;
+
+        deliveryOptionsHtml += `
+
+        <div class="delivery-option">
+
+            <input type="radio" ${isChecked ? 'checked' : ''} class="delivery-option-input" name="delivery-option-${matchingProduct.id}" data-date="${deliveryDate}">
+            <div>
+                <div class="delivery-option-date"> ${deliveryDate} </div>
+                <div class="delivery-option-price"> ${deliveryCost} Shipping </div>
+            </div>
+
+        </div>
+
+    `;
+
+    });
+
+    return deliveryOptionsHtml;
 }
