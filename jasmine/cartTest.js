d5537ccc1ceb8c5eb321e995d339a0f658cfab25
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-06 17:20:38 +0100

    test enhanced and completed 100%.

--- jasmine/cartTest.js
+++ jasmine/cartTest.js
@@ -36,9 +36,18 @@ describe('test suite: adds products to cart', () => {
             </div>
         `;
         const button = testContainer.querySelector('.add-to-cart-button');
+        const selectedValue = Number(button.parentElement.querySelector('select').value);
+
+        console.log(selectedValue);
 
         updateQuantity('15b6fc6f-327a-4ec4-896f-486349e85a3d', button);
+
         expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
+        expect(cart[0].quantity).toEqual(selectedValue);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+
+        testContainer.innerHTML = '';
     });
 
 });
