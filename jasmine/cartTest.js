3a6cd98b489b0831d35dd6a8dd5a717bdfa90f26
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-06 17:47:42 +0100

    added beforeEach feature to minimize code size.

--- jasmine/cartTest.js
+++ jasmine/cartTest.js
@@ -2,18 +2,12 @@ import { cart, updateQuantity, loadCartFromStorage } from "../data/cart.js";
 
 describe('test suite: adds products to cart', () => {
 
-    it('adds a new product', () => {
+    let testContainer;
+    let button;
+    let selectedValue;
 
-        spyOn(localStorage, 'setItem');
-        // prevent localStorage from modifying localStorage in the main file
-
-        spyOn(localStorage, 'getItem').and.callFake(() => {
-            return JSON.stringify([]);
-        });
-
-        loadCartFromStorage(); // to restore the expected value of cart in the main file
-
-        const testContainer = document.querySelector('.test-container');
+    beforeEach(() => {
+        testContainer = document.querySelector('.test-container');
         testContainer.innerHTML = `
             <div class="product-container">
                 <select>
@@ -33,8 +27,19 @@ describe('test suite: adds products to cart', () => {
                 </button>
             </div>
         `;
-        const button = testContainer.querySelector('.add-to-cart-button');
-        const selectedValue = Number(button.parentElement.querySelector('select').value);
+        button = testContainer.querySelector('.add-to-cart-button');
+        selectedValue = Number(button.parentElement.querySelector('select').value);
+    });
+    it('adds a new product', () => {
+
+        spyOn(localStorage, 'setItem');
+        // prevent localStorage from modifying localStorage in the main file
+
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([]);
+        });
+
+        loadCartFromStorage(); // to restore the expected value of cart in the main file
 
         updateQuantity('15b6fc6f-327a-4ec4-896f-486349e85a3d', button);
 
@@ -62,29 +67,6 @@ describe('test suite: adds products to cart', () => {
         });
         
         loadCartFromStorage();
-
-        const testContainer = document.querySelector('.test-container');
-        testContainer.innerHTML = `
-            <div class="product-container">
-                <select>
-                    <option selected value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                </select>
-                <button class="add-to-cart-button button-primary" data-product-id = "15b6fc6f-327a-4ec4-896f-486349e85a3d">
-                    Add to Cart
-                </button>
-            </div>
-        `;
-        const button = testContainer.querySelector('.add-to-cart-button');
-        const selectedValue = Number(button.parentElement.querySelector('select').value);
 
         updateQuantity('15b6fc6f-327a-4ec4-896f-486349e85a3d', button);
 
