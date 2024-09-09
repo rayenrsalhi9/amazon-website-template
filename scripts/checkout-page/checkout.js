bf5e56955cdb4b97433592bfaafb96f7ff8acafb
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-09 22:55:48 +0100

    turned callback into a promise.

--- scripts/checkout-page/checkout.js
+++ scripts/checkout-page/checkout.js
@@ -3,7 +3,18 @@ import { generatePayment } from './payment.js';
 import { loadBackend } from '../../data/products.js';
 //import '../../data/backend-practice.js'
 
+new Promise((resolve) => {
+    loadBackend(() => {
+        resolve();
+    });
+}).then(() => {
+    renderOrderSummary();
+    generatePayment();
+});
+
+/*
 loadBackend(() => {
     renderOrderSummary();
     generatePayment();
 });
+*/
