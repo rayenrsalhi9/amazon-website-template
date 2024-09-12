1c3b66e34e26f0e754eba76184ab5597d8b007ca
Salhi Rayen <rayen.rsalhi9@gmail.com>
2024-09-12 13:35:49 +0100

    tracking page is now interactive.

--- scripts/tracking-page/tracking.js
+++ scripts/tracking-page/tracking.js
@@ -7,6 +7,7 @@ fetchBackend().then(() => {
 });
 
 function renderTrackingPage() {
+    
     displayCartQuantity();
     const trackingContainer = document.querySelector('.main');
 
@@ -22,8 +23,12 @@ function renderTrackingPage() {
 
         let matchingItem;
         let matchingProductsItem;
+        let matchingOrderId;
+
 
         orders.forEach(order => {
+
+            if (order.id === pageOrderId) matchingOrderId = order;
 
             order.products.forEach(product => {
 
@@ -31,12 +36,14 @@ function renderTrackingPage() {
 
             });
 
-            
         });
 
         products.forEach(element => {
             if (element.id === matchingItem.productId) matchingProductsItem = element;
         });
+
+        let width = calculateWidth(matchingItem, matchingOrderId);
+        width = width < 100 ? width : 100;
 
         const arrivingDate = dayjs(matchingItem.estimatedDeliveryTime).format('dddd, MMMM D');
 
@@ -63,19 +70,19 @@ function renderTrackingPage() {
                 <img class="product-image" src="${matchingProductsItem.image}">
 
                 <div class="progress-labels-container">
-                    <div class="progress-label">
+                    <div class="progress-label" data-min-width="0" data-max-width="49">
                         Preparing
                     </div>
-                    <div class="progress-label current-status">
+                    <div class="progress-label" data-min-width="50" data-max-width="99">
                         Shipped
                     </div>
-                    <div class="progress-label">
+                    <div class="progress-label" data-min-width="100" data-max-width="100">
                         Delivered
                     </div>
                 </div>
 
                 <div class="progress-bar-container">
-                    <div class="progress-bar"></div>
+                    <div class="progress-bar" style="width: ${width}%"></div>
                 </div>
 
             </div>
@@ -83,6 +90,7 @@ function renderTrackingPage() {
 
         trackingContainer.innerHTML = trackingHtml;
 
+        selectCurrentLabel(width);
     }
 
     function displayCartQuantity() {
@@ -92,5 +100,30 @@ function renderTrackingPage() {
             quantity += product.quantity;
         });
         cartQuantity.innerHTML = quantity;
+    }
+
+    function calculateWidth(matchingItem, matchingOrderId) {
+
+        const currentTime = dayjs();
+        const deliveryTime = dayjs(matchingItem.estimatedDeliveryTime);
+        const orderTime = dayjs(matchingOrderId.orderTime);
+
+        const width = ((currentTime - orderTime) / (deliveryTime - orderTime)) * 100;
+        
+        return width;
+
+    }
+
+    function selectCurrentLabel(width) {
+
+        const progessLabels = document.querySelectorAll('.progress-label');
+
+        progessLabels.forEach(label => {
+
+            const minWidth = label.dataset.minWidth;
+            const maxWidth = label.dataset.maxWidth;
+
+            if (width >= minWidth && width <= maxWidth) label.classList.add('current-status');
+        });
     }
 }
